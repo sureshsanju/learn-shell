7961e06d4221adca081f62ca746260ce5b21f12c
sureshdevops <suresh19937284@gmail.com>
2023-07-26 00:53:31 +0530

    sanju23

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,18 @@
+location=$(pwd)
 
-script_location=$(pwd)
 
-yum install ngnix -y
-systemctl enable ngnix
-systemctl start ngnix
-rm -rf /usr/share/ngnix/html/*
+yum install nginx -y 
+
+systemctl enable nginx 
+systemctl start nginx 
+
+rm -rf /usr/share/nginx/html/* 
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-cp ${script-location}/sanju/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${location}/sanju/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-
-systemctl restart nginx
+systemctl restart nginx 
