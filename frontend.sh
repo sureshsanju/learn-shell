e91e88be80e3f26e78bf958dfb7d463b795a70f5
sureshsanju <127091157+sureshsanju@users.noreply.github.com>
2023-07-26 00:15:30 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ script_location=$(pwd)
 yum install ngnix -y
 systemctl enable ngnix
 systemctl start ngnix
-rm-rf /usr/share/ngnix/html/*
+rm -rf /usr/share/ngnix/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
