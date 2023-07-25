d5894dd7c2cb9c088d1d3a1eb1bc7f2ec0f8169d
sureshdevops <suresh19937284@gmail.com>
2023-07-26 00:07:43 +0530

    editedpush

--- frontend.sh
+++ frontend.sh
@@ -7,5 +7,5 @@ rm-rf /usr/share/ngnix/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-cp $(script-location)files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp $(script-location)/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
