30a95772e044c4eed64ea402fc6dd5a13d30d7ac
sureshdevops <suresh19937284@gmail.com>
2023-07-26 04:48:47 +0530

    sanju23

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ cp ${location}/sanju/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y
 
-sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod 
 systemctl start mongod 
